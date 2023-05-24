c6111422af238b37f6f094a0a7390d659022bdec
yurign000 <yurign000@gmail.com>
2023-05-24 16:45:17 -0300

    feat: add shiny and female modes

--- src/components/pokedex/left-side/LeftSide.jsx
+++ src/components/pokedex/left-side/LeftSide.jsx
@@ -1,10 +1,40 @@
+import { useState,useEffect } from 'react';
 import './leftSide.css'
 
 export default function LeftSide(props){
+    const [sprite, setSprite] = useState();
+    const [isShiny, setIsShiny] = useState(false);
+    const [isMale, setIsMale] = useState(true);
+
+    const spriteIndex = ()=>{
+        if(!isShiny && isMale)  return 'front_default';
+        if(isShiny  && isMale)  return 'front_shiny';
+        if(!isShiny && !isMale) return 'front_female';
+        else                    return 'front_shiny_female';
+    }
+
     const setDisplayVisible = ()=>{
         return props?.pokemon ? {display:'flex'} : {display:'none'};
     };
 
+    const handleIsShiny = ()=>{
+        setIsShiny(!isShiny);
+    }
+    const handleIsMale = ()=>{
+        setIsMale(!isMale);
+    }
+
+    useEffect(()=>{
+        if(props.pokemon){
+            if(props.pokemon.sprites[spriteIndex()] == null){
+                setIsMale(true);
+                console.log('sem versão feminina')
+            }
+
+            setSprite(props.pokemon.sprites[spriteIndex()]);
+        }
+    },[props.pokemon,isMale,isShiny])
+        
     return(
         <section className="left-side">
             <div className='top-part'></div>
@@ -15,14 +45,14 @@ export default function LeftSide(props){
 
 
                     <div className="pokemon-screen">
-                        <div className='shiny-button'>
-                            <button>💡</button>
+                        <div className='shiny-button' style={setDisplayVisible()}>
+                            <button onClick={()=>handleIsShiny()}>💡</button>
                         </div>
                         <div className='name'>
                             <p>{props?.pokemon?.name}</p>
                         </div>
                         <div className='img-screen'>
-                            <img src={props.pokemon?.sprites?.front_default}></img>
+                            <img src={sprite} onClick={()=>handleIsMale()}></img>
                         </div>
                     </div>
                     
